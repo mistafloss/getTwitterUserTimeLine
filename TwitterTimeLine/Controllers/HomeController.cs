b85514144469da4bf77a0e8f078d0af176e7ea2c
Uzoma <u.umekwe@gmail.com>
2016-06-03 19:24:24 +0100

    initial commit

---
+++ TwitterTimeLine/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TweetSharp;
+using System.Configuration;
+
+namespace TwitterTimeLine.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string twitterUsername)
+        {
+            //Retrieve consumer key and consumer secret
+            string twitterConsumerKey = ConfigurationManager.AppSettings["TwitterConsumerKey"].ToString();
+            string twitterConsumerSecret = ConfigurationManager.AppSettings["TwitterConsumerSecret"].ToString();
+            var twitterService = new TwitterService(twitterConsumerKey, twitterConsumerSecret);
+
+            //Authenticate twitter service using Access token and Access token secret
+            var twitterAccessToken = ConfigurationManager.AppSettings["TwitterAccessToken"].ToString();
+            var twitterAccessTokenSecret = ConfigurationManager.AppSettings["TwitterAccessTokenSecret"].ToString();
+
+            twitterService.AuthenticateWith(twitterAccessToken, twitterAccessTokenSecret);
+
+            IEnumerable<TwitterStatus> tweets = twitterService.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions { ScreenName = twitterUsername, Count = 10 });
+
+            ViewBag.Tweets = tweets;
+
+            return View();
+            //return Json(tweets, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
